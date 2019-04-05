11a56c6cae920ffd44968cb47d18c7d787368d18
theecn <310659947@qq.com>
2019-04-05 16:27:02 +0800

    添加 icon

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,51 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-    export default {}
+    export default {
+        props:['icon','iconPosition']
+    }
 </script>
 <style lang="scss">
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
-        padding: 0 1em;
+        padding: 0 .9em;
         background: var(--button-bg);
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover {
             border-color: var(--border-color-hover);
         }
-
         &:active {
             background-color: var(--button-active-bg);
         }
-
         &:focus {
             outline: none;
         }
+        > .content{order: 2}
+        > .icon{order: 1;margin-right:.3em;}
+        &.icon-right{
+            > .content{
+                order: 1;
+            }
+            > .icon{
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+        }
+
     }
 </style>
