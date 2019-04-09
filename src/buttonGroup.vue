c65867378b33e1a765e63999f2576a10883b36f9
theecn <310659947@qq.com>
2019-04-09 10:38:17 +0800

    fixed bug about the buttongroup

--- src/buttonGroup.vue
+++ src/buttonGroup.vue
@@ -4,7 +4,16 @@
     </div>
 </template>
 <script>
-
+    export default {
+        mounted() {
+            for (let node of this.$el.children){
+                let name = node.nodeName.toLowerCase()
+                if (name!=='button'){
+                    console.warn(`gad-button-group 的子元素应当都为 gad-button,但是你写的是${name}`)
+                }
+            }
+        }
+    }
 </script>
 <style lang="scss">
     .g-button-group {
@@ -12,7 +21,9 @@
         vertical-align: middle;
         > .g-button {
             border-radius:0;
-            margin-left:-1px;
+            &:not(:first-child){
+                margin-left:-2px;
+            }
             &:first-child{
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
