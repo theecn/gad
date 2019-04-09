58f12c273b63d0d965fa4fc988ade5caf35a1852
theecn <310659947@qq.com>
2019-04-09 10:06:34 +0800

    finish button-group created

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './buttonGroup'
 
 Vue.component('gad-button',Button)
 Vue.component('gad-icon',Icon)
-
+Vue.component('gad-button-group',ButtonGroup)
 new Vue({
     el:'#app',
     data:{
-        message:'hello',
         loading1:false,
         loading2:true,
         loading3:false
